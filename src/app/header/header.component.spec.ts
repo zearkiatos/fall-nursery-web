ad428cc2034abc24c320a1535c62dd8c09fa84d0
zearkiatos <caprilespe@outlook.com>
2023-04-30 08:01:51 -0400

    Adding tests

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -9,6 +9,7 @@ import { HeaderComponent } from './header.component';
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -22,9 +23,20 @@ describe('HeaderComponent', () => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a header with a image', () => {
+
+    expect(debug.query(By.css('header'))).toHaveSize(1);
+    expect(debug.query(By.css('header>h1'))).toHaveSize(1);
+    debug.queryAll(By.css('header>h1')).forEach((h1, i)=>{
+      expect(h1.nativeElement.textContent).toContain('Vivero El Otoño 🍁')
+    });
+    expect(debug.query(By.css('header>div.image'))).toHaveSize(1);
   });
 });
--- src/app/plant/plant-list/plant-list.component.spec.ts
+++ src/app/plant/plant-list/plant-list.component.spec.ts
@@ -1,15 +1,18 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { faker } from '@faker-js/faker';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { PlantService } from '../plant.service';
+import Plant from '../plant';
 
 import { PlantListComponent } from './plant-list.component';
 
 describe('PlantListComponent', () => {
   let component: PlantListComponent;
   let fixture: ComponentFixture<PlantListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -23,9 +26,29 @@ describe('PlantListComponent', () => {
     fixture = TestBed.createComponent(PlantListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+
+    for(let i = 0; i < 3; i++) {
+      const plant = new Plant(
+        i,
+        faker.lorem.word(),
+        faker.lorem.word(),
+        faker.lorem.word(),
+        parseInt(faker.random.numeric()),
+        faker.lorem.word(),
+        faker.lorem.sentence()
+      );
+      component.plants.push(plant);
+    }
+    fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render a list of plants with 3 rows', () => {
+    expect(debug.queryAll(By.css('table.table>tbody>tr'))).toHaveSize(3)
+  })
 });
